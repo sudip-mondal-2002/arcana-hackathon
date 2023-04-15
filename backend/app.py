c9b7982fd446e61575c62a1933f3e3962bff9643
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-04-16 01:46:19 +0530

    add sentiment analysis

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 import yfinance as yf
 from flask_cors import CORS
+from transformers import pipeline
+import feedparser
+
 app = Flask(__name__)
 CORS(app)
+classifier = pipeline("sentiment-analysis",model="soleimanian/financial-roberta-large-sentiment")
 
 @app.route('/company/<ticker>')
 def getCompanyDetails(ticker: str)->dict:
@@ -19,13 +23,27 @@ def getCompanyDetails(ticker: str)->dict:
     officers = {}
     for i in company.info["companyOfficers"]:
         officers[i["title"]] = i["name"]
-    history = company.history(period="max").to_dict("list")["Close"]
+    # history = company.history(period="max").to_dict("list")["Close"]
 
-    return {
-        "info": info,
-        "officers": officers,
-        "history": history
-    }
+    news_url = f"https://feeds.finance.yahoo.com/rss/2.0/headline?s={ticker}&region=US&lang=en-US"
+    news_feed = feedparser.parse(news_url)
+    news = []
+    for i in news_feed.entries:
+        if "summary" in i:
+            news.append(i["summary"])
+
+    sentiment_scores = []
+    for n in news:
+        result = classifier(n)[0]
+        sentiment_scores.append(result['score'])
+
+    # return {
+    #     "info": info,
+    #     "officers": officers,
+    #     "history": history
+    # }
+
+    return sentiment_scores
 
 
 
