dc1e531bb4b5e78fd51be6c20ee14cb3fc9eb85c
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2023-04-15 21:33:34 +0000

    add sentiment analysis

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ import feedparser
 
 app = Flask(__name__)
 CORS(app)
+
 classifier = pipeline("sentiment-analysis",model="soleimanian/financial-roberta-large-sentiment")
 
 @app.route('/company/<ticker>')
@@ -23,27 +24,39 @@ def getCompanyDetails(ticker: str)->dict:
     officers = {}
     for i in company.info["companyOfficers"]:
         officers[i["title"]] = i["name"]
-    # history = company.history(period="max").to_dict("list")["Close"]
+    history = company.history(period="max").to_dict("list")["Close"]
 
     news_url = f"https://feeds.finance.yahoo.com/rss/2.0/headline?s={ticker}&region=US&lang=en-US"
     news_feed = feedparser.parse(news_url)
     news = []
     for i in news_feed.entries:
         if "summary" in i:
-            news.append(i["summary"])
+            news.append([i["title"],i["summary"]])
 
-    sentiment_scores = []
+    sentiment_score_nom = 0
+    sentiment_score_denom = 0
+    news_to_return = []
     for n in news:
-        result = classifier(n)[0]
-        sentiment_scores.append(result['score'])
+        result = classifier(n[1])[0]
+        news_to_return.append({
+            "title": n[0], 
+            "summary": n[1], 
+            "sentiment": result["label"]
+        })
+        if result['label'] == 'positive':
+            sentiment_score_nom += result["score"]**3
+        elif result['label'] == 'negative':
+            sentiment_score_nom -= result["score"]**3
+        sentiment_score_denom += result["score"]
+    sentiment_score = (sentiment_score_nom/sentiment_score_denom)**0.5
+    return {
+        "info": info,
+        "officers": officers,
+        # "history": history,
+        "sentiment": sentiment_score,
+        "news": news_to_return
+    }
 
-    # return {
-    #     "info": info,
-    #     "officers": officers,
-    #     "history": history
-    # }
-
-    return sentiment_scores
 
 
 
