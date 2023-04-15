99ef5f03495da2c726ce33c9a35dc8e89ac75fa3
NewbieNate <105610628+NewbieNate@users.noreply.github.com>
2023-04-15 22:59:02 +0000

    complete utils

--- backend/utils.py
+++ backend/utils.py
@@ -16,11 +16,23 @@ def save_all_transcript_sentiments():
         Saves all scrapped trsncript sentiments to a file
     """
     result = {}
+    try:
+        with open(TICKER_SENTIMENT_DICT_PATH, 'rb') as input:
+            result = pickle.load(input)
+    except:
+        pass
+    counter = 0
+
+    print(result)
+
     for ticker in TICKERS_LIST:
         result[ticker] = get_sent_from_ticker(ticker)
-        print("Done results for ", ticker, result[ticker], end='\n*************************')
+        print("Done results for ", ticker)
+        counter += 1
+        if counter%10==0:
+            with open(TICKER_SENTIMENT_DICT_PATH, 'wb') as output:
+                pickle.dump(result, output, pickle.HIGHEST_PROTOCOL)
+            print("Ends at counter =", counter)
+            print("Ends at Ticker =", ticker)
         
-    with open(TICKER_SENTIMENT_DICT_PATH, 'wb') as output:
-        pickle.dump(result, output, pickle.HIGHEST_PROTOCOL)
-
 save_all_transcript_sentiments()
